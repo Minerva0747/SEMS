3b17b1f80a46ac02863e7c86b3c253627b76a728
Muhammad Amirul Iman <1171303225@student.mmu.edu.my>
2020-12-09 22:54:07 +0800

    Event

---
+++ routes/event.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { check, validationResult} = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const auth = require("./../middleware/auth");
+
+const User = require("../model/User");
+const Event= require("../model/Event");
+
