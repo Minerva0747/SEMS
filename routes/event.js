b855f18519f314083405da4ba1eebc236d989458
Muhammad Amirul Iman <1171303225@student.mmu.edu.my>
2021-01-13 11:48:17 +0800

    Back End

--- routes/event.js
+++ routes/event.js
@@ -52,6 +52,85 @@ router.post("/create", auth, async (req, res) => {
 
   });
 
+  router.put("/:id", auth, async (req, res) => {
+    let event
+    try{
+        updateQuery = {};
+
+        if(req.body.name)
+        {
+          updateQuery.eventName = req.body.eventName;
+        }
+    
+        if(req.body.organizedBy)
+        {
+          updateQuery.organizedBy = req.body.organizedBy;
+        }
+    
+        if(req.body.eventStartDate)
+        {
+          updateQuery.eventStartDate = req.body.eventStartDate;
+        }
+    
+        if(req.body.eventEndDate)
+        {
+          updateQuery.eventEndDate = req.body.eventEndDate;
+        }
+    
+        if(req.body.venue)
+        {
+          updateQuery.venue = req.body.venue;
+        }
+    
+        if(req.body.eventActivities)
+        {
+          updateQuery.eventActivities = req.body.eventActivities;
+        }
+
+        if(req.body.description)
+        {
+          updateQuery.description = req.body.description;
+        }
+        
+   await User.findByIdAndUpdate(
+      req.params.id,
+      updateQuery, {new:true},
+      function(err, result) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.redirect("/myEvent");
+        }
+      }
+    );
+
+    } catch{
+        if(event == null){
+            res.redirect('/');
+        } else{
+            res.redirect('/');
+        }
+    }
+});
+
+
+  router.delete("/:id", auth, async (req, res) => {
+      let event
+      try{
+          event = await Event.findById(req.params.id);
+          await event.remove();
+          res.redirect('/');
+      } catch{
+          if(event == null){
+              res.redirect('/')
+          } else{
+              res.redirect('/')
+          }
+      }
+  });
+
+
+
 
 
 
